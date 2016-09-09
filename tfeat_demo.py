a494b0ce032df047e84422a6733031d4c651d04a
edgarriba <edgar.riba@gmail.com>
2016-09-09 17:51:18 +0200

    update code with classes and drawing routines

--- tfeat_demo.py
+++ tfeat_demo.py
@@ -1,138 +1,173 @@
-import numpy as np
-import lutorpy as lua
-require('nn')
-
+import sys
 import cv2
 
-def extract_patches(img, kpts):
-    patches = []  
-    for kp in kpts:
-        sub = cv2.getRectSubPix(img, (int(kp.size*1.3), int(kp.size*1.3)), kp.pt)
-        res = cv2.resize(sub, (32, 32), interpolation = cv2.INTER_CUBIC)
-        m   = np.ones((32,32), dtype=np.uint8) * cv2.mean(res)[0]
-        # subtract mean
-        patch = (res - m)
-        patches.append(patch)
-    return np.asarray(patches)
+import numpy as np
+import lutorpy as lua
 
-def compute_descriptors(img, kpts, net):
-    print 'Computing descriptor from ', len(kpts), ' kpts'
-
-    N = len(kpts)
+class LutorpyNet:
+    """Wrapper to Torch for loading models"""
     # TODO: check codes for a bigger batch size.
     # Now it only works with size of one.
-    batch_sz = 1
+    batch_sz = 5000
+    # TFeat number of input channels
+    input_channels = 1
+    # TFeat image input size
+    input_sz = 32
+    # TFeat descriptor size
+    descriptor_sz = 128
 
-    # extract the patches given the keypoints
-    patches = extract_patches(img, kpts)
-    assert N == len(patches)
+    def __init__(self, model_file):
+        require('nn')
+        require('cudnn')
+        self.net = torch.load(model_file)
+        self.ones_arr = np.ones((self.input_sz, self.input_sz), dtype=np.uint8)
 
-    patches_t = torch.fromNumpyArray(patches)
-    patches_t._view(N,1,32,32)
+    def extract_patches(self, img, kpts):
+        patches = []  
+        for kp in kpts:
+            sub = cv2.getRectSubPix(img, (int(kp.size*1.3), int(kp.size*1.3)), kp.pt)
+            res = cv2.resize(sub, (self.input_sz, self.input_sz))
+            # subtract mean
+            nmean = res - (self.ones_arr * cv2.mean(res)[0])
+            nmean = nmean.reshape(self.input_channels, self. input_sz, self.input_sz)
+            patches.append(nmean)
+        return np.asarray(patches)
 
-    patches_t   = patches_t._split(batch_sz)
-    descriptors = []
+    def compute(self, img, kpts):
+        # number of keypoints
+        N = len(kpts)
+        #print 'Compute descriptors from ', N, ' keypoints'
 
-    for i in range(N / batch_sz):
-        print patches_t[i]._size()
+        # extract the patches given the keypoints
+        patches = self.extract_patches(img, kpts)
+        assert N == len(patches)
 
-        # infere Torch network
-        prediction_t = net._forward(patches_t[i]._float())
-        
-        # Cast TorchTensor to NumpyArray and append to results
-        prediction = prediction_t.asNumpyArray()
+        # convert numpy array to torch tensor
+        patches_t = torch.fromNumpyArray(patches)
+        patches_t._view(N, self.input_channels, self.input_sz, self.input_sz)
 
-        # add the current prediction to the buffer
-        descriptors.append(prediction)
-        
-        #print descriptors
-        #raw_input("Press Enter to continue...")
+        # split patches into batches
+        patches_t   = patches_t._split(self.batch_sz)
+        descriptors = []
 
-    return np.float32(np.asarray(descriptors))
+        for i in range(int(np.ceil(float(N) / self.batch_sz))):
+            # infere Torch network
+            prediction_t = self.net._forward(patches_t[i]._cuda())
+            
+            # Cast TorchTensor to NumpyArray and append to results
+            prediction = prediction_t.asNumpyArray()
 
+            # add the current prediction to the buffer
+            descriptors.append(prediction)
 
-MIN_MATCH_COUNT = 10
+        return np.float32(np.asarray(descriptors).reshape(N, self.descriptor_sz))
 
-TORCH_FILE = '/home/eriba/software/bmva/networks/torch/tfeat/margin_star/tfeat_test.t7'
+def compute_and_draw_homography(frame, img, good, kp1, kp2, descriptor):
+    src_pts = np.float32([ kp1[m.queryIdx].pt for m in good ]).reshape(-1,1,2)
+    dst_pts = np.float32([ kp2[m.trainIdx].pt for m in good ]).reshape(-1,1,2)
 
-net = torch.load(TORCH_FILE)
-print net
+    M, mask = cv2.findHomography(src_pts, dst_pts, cv2.RANSAC, 5.0)
+    matchesMask = mask.ravel().tolist()
 
-# start opencv stuff
+    h,w,d = img.shape
+    pts = np.float32([ [0,0],[0,h-1],[w-1,h-1],[w-1,0] ]).reshape(-1,1,2)
+    dst = cv2.perspectiveTransform(pts, M)
 
-cap = cv2.VideoCapture('/home/eriba/Videos/Webcam/roibos_video.webm')
-#cap = cv2.VideoCapture(0)
-
-# Initiate ORB detector
-det = cv2.ORB_create()
-#det = cv2.xfeatures2d.SIFT_create()
-
-FLANN_INDEX_KDTREE = 0
-index_params = dict(algorithm = FLANN_INDEX_KDTREE, trees = 5)
-search_params = dict(checks = 50)
- 
-flann = cv2.FlannBasedMatcher(index_params, search_params)
-bf = cv2.BFMatcher(cv2.NORM_L2, crossCheck=True)
-
-img = cv2.imread('/home/eriba/Pictures/Webcam/roibos.jpg')
-img = cv2.cvtColor(img, cv2.COLOR_BGR2GRAY)
-
-kp1, des1 = det.detectAndCompute(img, None)
-des1 = compute_descriptors(img, kp1, net)
-
-while(cap.isOpened()):
-    # Capture frame-by-frame
-    ret, frame = cap.read()
-
-    # Our operations on the frame come here
-    gray = cv2.cvtColor(frame, cv2.COLOR_BGR2GRAY)
-    print gray.shape
-
-    # find the keypoints and the descriptors
-    kp2, des2 = det.detectAndCompute(gray, None)
-    des2 = compute_descriptors(img, kp2, net)
-    print 'Desc1: ', des1.shape, ' type: ', des1.dtype
-    print 'Desc2: ', des2.shape, ' type: ', des2.dtype
-   
-    #matches = flann.knnMatch(des1, des2, k=2)
-    good = bf.match(des1, des2)
-    print 'Good matches ', len(good)
-
-    # store all the good matches as per Lowe's ratio test.
-#    good = []
-#    for m,n in matches:
-#        if m.distance < 0.7*n.distance:
-#            good.append(m)
-
-    if len(good)>MIN_MATCH_COUNT:
-        src_pts = np.float32([ kp1[m.queryIdx].pt for m in good ]).reshape(-1,1,2)
-        dst_pts = np.float32([ kp2[m.trainIdx].pt for m in good ]).reshape(-1,1,2)
-    
-        M, mask = cv2.findHomography(src_pts, dst_pts, cv2.RANSAC, 5.0)
-        matchesMask = mask.ravel().tolist()
-    
-        h,w = img.shape
-        #h,w,d = img.shape
-        pts = np.float32([ [0,0],[0,h-1],[w-1,h-1],[w-1,0] ]).reshape(-1,1,2)
-        dst = cv2.perspectiveTransform(pts, M)
-    
-        img2 = cv2.polylines(frame, [np.int32(dst)], True, 255, 3, cv2.LINE_AA)
-    
+    if descriptor == 'TFEAT':
+        colour    = (255,0,0)
+        thickness = 3
+        position  = (480, 50)
     else:
-        print "Not enough matches are found - %d/%d" % (len(good), MIN_MATCH_COUNT)
-        matchesMask = None
+        colour    = (0,0,255)
+        thickness = 1
+        position  = (480, 90)
 
-    draw_params = dict(matchColor = (0,255,0), # draw matches in green color
-                       singlePointColor = None,
-                       matchesMask = matchesMask, # draw only inliers
-                       flags = 2)
+    img2 = cv2.polylines(frame, [np.int32(dst)], True, colour, thickness, cv2.LINE_AA)
     
-    img3 = cv2.drawMatches(img, kp1, frame, kp2, good, None, **draw_params)
+    font = cv2.FONT_HERSHEY_SIMPLEX
+    cv2.putText(img2, descriptor, position, font, 1, colour, 2)
 
-    # Display the resulting frame
-    cv2.imshow('frame', img3)
-    if cv2.waitKey(20) & 0xFF == ord('q'):
-        break
- 
-# When everything done, release the capture
-cv2.destroyAllWindows()
+    return img2, matchesMask
+
+def main(torch_file, video_file, object_img):
+
+    # the required minimum number of good matches to 
+    # compute the homography between the object image
+    # and the current frame.
+    MIN_MATCH_COUNT = 10
+
+    # create CNN descriptor
+    net = LutorpyNet(torch_file)
+
+    # start opencv stuff
+
+    cap = cv2.VideoCapture(video_file)
+    #cap = cv2.VideoCapture(0)
+
+    fourcc = cv2.VideoWriter_fourcc(*'XVID')
+    out = cv2.VideoWriter('output.avi',fourcc, 10.0, (640,480))
+
+    # initialise ORB detector
+    det = cv2.ORB_create(1000)
+    des = cv2.ORB_create(1000)
+
+    # initialise matcher
+    bf = cv2.BFMatcher(cv2.NORM_L2, crossCheck=True)
+
+    # read object image and compute descriptors
+    img = cv2.imread(object_img)
+    gray = cv2.cvtColor(img, cv2.COLOR_BGR2GRAY)
+
+    kp1   = det.detect(gray, None)
+    des11 = net.compute(gray, kp1)
+    _ , des12 = des.compute(gray, kp1)
+
+    # video loop
+
+    while(cap.isOpened()):
+        # Capture frame-by-frame
+        ret, frame = cap.read()
+
+        # Our operations on the frame come here
+        gray = cv2.cvtColor(frame, cv2.COLOR_BGR2GRAY)
+
+        # find the keypoints and the descriptors
+        kp2   = det.detect(gray, None)
+        des21 = net.compute(gray, kp2)
+        _ , des22 = des.compute(gray, kp2)
+   
+        # match features
+        good1 = bf.match(des11, des21) 
+        good2 = bf.match(des12, des22) 
+
+        # comptue the homography and it to the current frame
+
+        if len(good1) > MIN_MATCH_COUNT:
+            compute_and_draw_homography(frame, img, good1, kp1, kp2, 'TFEAT')
+        else:
+            print "Not enough matches are found with TFEAT - %d/%d" % (len(good1), MIN_MATCH_COUNT)
+
+        if len(good2) > MIN_MATCH_COUNT:
+            compute_and_draw_homography(frame, img, good2, kp1, kp2, 'ORB')
+        else:
+            print "Not enough matches are found with SIFT - %d/%d" % (len(good2), MIN_MATCH_COUNT)
+
+        # save frame to video
+        out.write(frame)
+
+        # Display the resulting frame
+        cv2.imshow('frame', frame)
+        if cv2.waitKey(1) & 0xFF == ord('q'):
+            break
+    
+    # When everything done, release the capture
+    cv2.destroyAllWindows()
+
+
+if __name__ == '__main__':
+    if len(sys.argv) < 3:
+        print 'Not enough arguments.\n' \
+              'Usage: python tfeat_demo.py torch_file.t7 video_file.webm object_img.png'
+        sys.exit(0)
+
+    main(sys.argv[1], sys.argv[2], sys.argv[3]) 
